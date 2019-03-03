c48f3a6fc4af1d29029f9d6bd74d33e79da9a325
Zane Russell <zanerussell@zanes-mbp.lan>
2019-03-03 13:44:26 -0700

    Updates

---
+++ precache-manifest.985f78469f8e9d743179b9654b83ec87.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f03fe97195c85196a98015d615d63cb",
+    "url": "/kate-dinsmore/static/media/twitter.0f03fe97.svg"
+  },
+  {
+    "revision": "d5e047b94d374e505058",
+    "url": "/kate-dinsmore/static/css/main.4e920a56.chunk.css"
+  },
+  {
+    "revision": "5e27992646423aee8eb9",
+    "url": "/kate-dinsmore/static/js/runtime~main.5e279926.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/kate-dinsmore/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "373bbea13e1d9697b9104fe7c1e5323f",
+    "url": "/kate-dinsmore/static/media/kieldrum.373bbea1.jpg"
+  },
+  {
+    "revision": "cc02fd64033fc774d0458f98e00d0d0e",
+    "url": "/kate-dinsmore/static/media/albuminsert.cc02fd64.jpg"
+  },
+  {
+    "revision": "aa19a890ed532e96ae14a9f43c47fd5f",
+    "url": "/kate-dinsmore/static/media/facebook.aa19a890.svg"
+  },
+  {
+    "revision": "d5e047b94d374e505058",
+    "url": "/kate-dinsmore/static/js/main.d5e047b9.chunk.js"
+  },
+  {
+    "revision": "39a26959b8c252f24f9af31d2e05c1db",
+    "url": "/kate-dinsmore/static/media/instagram.39a26959.svg"
+  },
+  {
+    "revision": "d3c6fa229c3fc946312183a4ba7b1c07",
+    "url": "/kate-dinsmore/static/media/albumrelease.d3c6fa22.jpg"
+  },
+  {
+    "revision": "dedc01909ab78ddbfc76ef108a668fa8",
+    "url": "/kate-dinsmore/static/media/facebookwhite.dedc0190.svg"
+  },
+  {
+    "revision": "447540e107267c8406e68fcd09c780ec",
+    "url": "/kate-dinsmore/static/media/twitterwhite.447540e1.svg"
+  },
+  {
+    "revision": "1c0000aa2364d2d547ff8c664be72488",
+    "url": "/kate-dinsmore/static/media/instagramwhite.1c0000aa.svg"
+  },
+  {
+    "revision": "8cc28ac24bac850f42b2766186dd8eda",
+    "url": "/kate-dinsmore/static/media/mailwhite.8cc28ac2.svg"
+  },
+  {
+    "revision": "d8ec9ba2fec00a7f71943b5c09f99c6c",
+    "url": "/kate-dinsmore/index.html"
+  }
+];
